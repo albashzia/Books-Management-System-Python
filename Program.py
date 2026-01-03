23955caffadb513673683f0e0408f0dbd46e9eed
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-03 08:28:38 +0500

    Replacing if with elif and enhancing the readability of the code using comments

--- Program.py
+++ Program.py
@@ -1,3 +1,4 @@
+# creating main lists to hold data in a parallel format
 titles = []
 authors = []
 genres = []
@@ -24,17 +25,17 @@ def search_book_by_genre():
 #A function to remove a book by taking a keyword and removing items having that keyword from the lists along with from authors and genres list 
 def remove_book(title_keyword):
     global titles, authors, genres
-
+    # creating new lists for updated data 
     new_titles = []
     new_authors = []
     new_genres = []
-
+    # looping over the whole lists
     for i in range(len(titles)):
         if title_keyword.lower() not in titles[i].lower():
             new_titles.append(titles[i])
             new_authors.append(authors[i])
             new_genres.append(genres[i])
-
+    # replacing old lists with new lists having updated data
     titles = new_titles
     authors = new_authors
     genres = new_genres
@@ -43,28 +44,29 @@ def remove_book(title_keyword):
 print("Welcome to Books Management System")
 choice = 0
 while choice != 4:
+    # displaying a menu
     print("1. Add a book")
     print("2. Search a book")
     print("3. Remove a book")
     print("4. Exit")
-
+    # taking user's choice
     choice = int(input("Enter your choice: "))
-
+    # proceeding for choice = 1
     if choice == 1:
         title = input("Enter the title of the book: ")
         author = input("Enter the name of the author: ")
         genre = input("Enter the genre of the book: ")
         add_book(title, author, genre )
-
-    if choice == 2:
+    # proceeding for choice = 1
+    elif choice == 2:
         searched_books = search_book_by_genre()
         for i in range(len(searched_books)):
             print("-",searched_books[i])
         print()
-
-    if choice == 3:
+    # proceeding for choice = 1
+    elif choice == 3:
         keyword = input("Enter the keyword to remove the books having that word: ")
         remove_book(keyword)
-
-    if choice == 4:
+    # proceeding for choice = 1
+    elif choice == 4:
         break
